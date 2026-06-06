2e92af42515fec30c39a6bfccd3f44c12f62de17
Fmasterpro27 <180668601+Fmasterpro27@users.noreply.github.com>
2026-06-06 14:15:26 +0530

    Release v0.2.0

---
+++ funkitpy/cli.py
@@ -0,0 +1,89 @@
+import argparse
+
+from .jokes import joke, dad_joke
+from .roasts import roast
+from .version import __version__
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="funkit",
+        description="FunKitPy - Random jokes, dad jokes and roasts",
+    )
+
+    parser.add_argument(
+        "-v",
+        "-V",
+        "--version",
+        action="store_true",
+        help="Show FunKitPy version",
+    )
+
+    subparsers = parser.add_subparsers(
+        dest="command",
+        help="Available commands",
+    )
+
+    subparsers.add_parser(
+        "joke",
+        help="Get a random joke",
+    )
+
+    subparsers.add_parser(
+        "dad-joke",
+        help="Get a random dad joke",
+    )
+
+    subparsers.add_parser(
+        "roast",
+        help="Get a random roast",
+    )
+
+    subparsers.add_parser(
+        "commands",
+        help="List all available commands",
+    )
+
+    args = parser.parse_args()
+
+    if args.version:
+        print(f"FunKitPy v{__version__}")
+        return
+
+    if args.command == "joke":
+        print(joke())
+
+    elif args.command == "dad-joke":
+        print(dad_joke())
+
+    elif args.command == "roast":
+        print(roast())
+
+    elif args.command == "commands":
+        print("""
+Available Commands:
+
+  joke         Get a random joke
+  dad-joke     Get a random dad joke
+  roast        Get a random roast
+  commands     Show all commands
+
+Flags:
+
+  -v, -V,
+  --version    Show version information
+
+Examples:
+
+  funkit joke
+  funkit dad-joke
+  funkit roast
+  funkit -v
+""")
+
+    else:
+        parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
