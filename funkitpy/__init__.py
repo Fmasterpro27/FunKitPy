3fe78469fdb571f1e2910f647782215f69f5d75b
Fmasterpro27 <180668601+Fmasterpro27@users.noreply.github.com>
2026-06-09 10:28:19 +0530

    Release v0.3.1

--- funkitpy/__init__.py
+++ funkitpy/__init__.py
@@ -1,15 +1,17 @@
 from .jokes import joke, dad_joke, get_joke, get_dad_joke
 from .roasts import roast
 from .quotes import quote, quote_data
-from .version import __version__
+from importlib.metadata import version
+
+__version__ = version("FunKitPy")
 
 __all__ = [
-    "joke", 
-    "dad_joke", 
+    "joke",
+    "dad_joke",
     "roast",
     "quote",
     "quote_data",
     "get_joke",
     "get_dad_joke",
-    "__version__"
+    "__version__",
 ]
